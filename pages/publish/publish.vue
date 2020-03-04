61fc1fea62eeaf52db5fa0fd8ee06fe3ea90079e
Leo <18304097708@163.com>
2020-03-04 17:08:54 +0800

    多图上传组件

--- pages/publish/publish.vue
+++ pages/publish/publish.vue
@@ -10,34 +10,14 @@
 			<textarea v-model="textareaText" placeholder="说点啥吧..."></textarea>
 		</view>
 		<!-- 图片上传 -->
-		<view class="uni-list list-pd">
-			<view class="uni-list-cell cell-pd">
-				<view class="uni-uploader">
-					<view class="uni-uploader-head">
-						<view class="uni-uploader-title">点击可预览选好的图片</view>
-						<view class="uni-uploader-info">{{imageList.length}}/9</view>
-					</view>
-					<view class="uni-uploader-body">
-						<view class="uni-uploader__files">
-							<block v-for="(image,index) in imageList" :key="index">
-								<view class="uni-uploader__file">
-									<image class="uni-uploader__img" :src="image" :data-src="image" @tap="previewImage"></image>
-								</view>
-							</block>
-							<view class="uni-uploader__input-box" v-if="imageList.length<9">
-								<view class="uni-uploader__input" @tap="chooseImage"></view>
-							</view>
-						</view>
-					</view>
-				</view>
-			</view>
-		</view>
+		<uploud-images @uploud = "uploud"></uploud-images>
 	</view>
 </template>
 
 <script>
 	import uniNavBar from '../../components/uni-nav-bar/uni-nav-bar.vue';
-	import permisionManager  from "@/common_js/permission.js"
+	import uploudImages from '../../components/common/uploud-images.vue';
+	import permision from "@/common_js/permission.js"
 	var sourceType = [
 		['camera'],
 		['album'],
@@ -50,19 +30,14 @@
 	]
 	export default {
 		components: {
-			uniNavBar
+			uniNavBar,
+			uploudImages
 		},
 		data() {
 			return {
 				textareaText: "",
-				
-				imageList: [],
-				sourceTypeIndex: 2,
-				sourceType: ['拍照', '相册', '拍照或相册'],
-				sizeTypeIndex: 2,
-				sizeType: ['压缩', '原图', '压缩或原图'],
-				countIndex: 8,
-				count: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+				imageList: []
 			}
 		},
 		methods: {
@@ -82,93 +57,8 @@
 					}
 				});
 			},
-			chooseImage: async function() {
-				
-				console.log(plus);
-				
-				// #ifdef APP-PLUS
-				// TODO 选择相机或相册时 需要弹出actionsheet，目前无法获得是相机还是相册，在失败回调中处理
-				if (this.sourceTypeIndex !== 2) {
-					let status = await this.checkPermission();
-					if (status !== 1) {
-						return;
-					}
-				}
-				// #endif
-				uni.chooseImage({
-					sourceType: sourceType[this.sourceTypeIndex],
-					sizeType: sizeType[this.sizeTypeIndex],
-					count: this.imageList.length + this.count[this.countIndex] > 9 ? 9 - this.imageList.length : this.count[this.countIndex],
-					success: (res) => {
-						this.imageList = this.imageList.concat(res.tempFilePaths);
-					},
-					fail: (err) => {
-						// #ifdef APP-PLUS
-						if (err['code'] && err.code !== 0 && this.sourceTypeIndex === 2) {
-							this.checkPermission(err.code);
-						}
-						// #endif
-						// #ifdef MP
-						uni.getSetting({
-							success: (res) => {
-								let authStatus = false;
-								switch (this.sourceTypeIndex) {
-									case 0:
-										authStatus = res.authSetting['scope.camera'];
-										break;
-									case 1:
-										authStatus = res.authSetting['scope.album'];
-										break;
-									case 2:
-										authStatus = res.authSetting['scope.album'] && res.authSetting['scope.camera'];
-										break;
-									default:
-										break;
-								}
-								if (!authStatus) {
-									uni.showModal({
-										title: '授权失败',
-										content: 'Hello uni-app需要从您的相机或相册获取图片，请在设置界面打开相关权限',
-										success: (res) => {
-											if (res.confirm) {
-												uni.openSetting()
-											}
-										}
-									})
-								}
-							}
-						})
-						// #endif
-					}
-				})
-			},
-			previewImage: function(e) {
-				var current = e.target.dataset.src
-				uni.previewImage({
-					current: current,
-					urls: this.imageList
-				})
-			},
-			async checkPermission(code) {
-				let type = code ? code - 1 : this.sourceTypeIndex;
-				let status = permisionManager.isIOS ? await permisionManager.requestIOS(sourceType[type][0]) :
-					await permisionManager.requestAndroid(type === 0 ? 'android.permission.CAMERA' :
-						'android.permission.READ_EXTERNAL_STORAGE');
-
-				if (status === null || status === 1) {
-					status = 1;
-				} else {
-					uni.showModal({
-						content: "没有开启权限",
-						confirmText: "设置",
-						success: function(res) {
-							if (res.confirm) {
-								permisionManager.gotoAppSetting();
-							}
-						}
-					})
-				}
-				return status;
+			uploud(arr) {
+				this.imageList = arr;
 			}
 		}
 	}
@@ -177,9 +67,5 @@
 <style>
 	.uni-textarea {
 		border: 1upx solid #EEEEEE;
-	}
-
-	.uni-uploader-info {
-		margin-right: 30upx;
 	}
 </style>
